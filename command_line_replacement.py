9c116ac8843410f344a8873af64e20373b1535f7
Hannah Joyce <93941057+HJoyce1@users.noreply.github.com>
2022-03-22 13:44:02 +0000

    cleaned up file

--- command_line_replacement.py
+++ command_line_replacement.py
@@ -6,26 +6,15 @@ Created on Mon Nov 22 11:57:07 2021
 """
 
 #import pw_20190502_hannahedit as pw_HJ
-from pw_1Dsinglefieldline_testing import polar_wind_outflow as pwm
+from pw_1Dsinglefieldline_march2 import polar_wind_outflow as pwm
 #import pw_plotting_tools_editt as pwpt
 
 #pw_HJ.polar_wind_outflow('jupiter',0.01,10000,3,33,0,0,1,1,'TestA')
-
-pwm('jupiter',0.01,10000,3,12,1,0,1,1,'fixrhotest1')
+pwm('jupiter',0.01,10000,3,24,0,0,1,1,'fix_S')
 # 0.01,10000,3,16,0,0,1,0
 
-#pwm('planet_name', dt, its, rs, lshell, FAC_flag, CF_flag, plots, saves, run_name)
-
-#pwpt.plot_me_quick(e_flux,zz,'xlabel','Distance','on')
-
-
-# print(pwm.e_flux)
-
 '''
-notes:  
+NOTES:
     
-    
-  currently n is initially a factor of 10 to low and drops off by another 10**2 by the end
-  is only changed by rho, so either changes in rho are the problem or the initial values
-    
+
 '''
