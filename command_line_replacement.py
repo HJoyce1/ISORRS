353fe6dec6820d618d68911f5f4ef4f254068ef3
Hannah Joyce <93941057+HJoyce1@users.noreply.github.com>
2022-03-29 10:03:31 +0100

    Update command_line_replacement.py

--- command_line_replacement.py
+++ command_line_replacement.py
@@ -7,11 +7,14 @@ Created on Mon Nov 22 11:57:07 2021
 
 #import pw_20190502_hannahedit as pw_HJ
 from pw_1Dsinglefieldline_march2 import polar_wind_outflow as pwm
-#import pw_plotting_tools_editt as pwpt
+from pw_20190502 import polar_wind_outflow as pwm2
+from pw_1Dsinglefieldline_FAC import polar_wind_outflow as pwm3
 
+# planet, time step, iterations, distance (RJ), Lshell, FAC?, CF?, plots?, saves?, name of file
 #pw_HJ.polar_wind_outflow('jupiter',0.01,10000,3,33,0,0,1,1,'TestA')
-pwm('jupiter',0.01,10000,3,24,0,0,1,1,'fix_S')
-# 0.01,10000,3,16,0,0,1,0
+#pwm('jupiter',0.01,10000,3,24,0,0,1,1,'breakpoint')
+#pwm2('jupiter',0.01,10000,3,24,0,0,1,1,'pw_2019test')
+pwm3('jupiter',0.01,10000,3,24,0,0,1,1,'results_test_8')
 
 '''
 NOTES:
