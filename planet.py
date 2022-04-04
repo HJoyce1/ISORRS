cf15585d6cb299d06e58c1fb349fc09aa57cdbc3
Hannah Joyce <93941057+HJoyce1@users.noreply.github.com>
2022-04-04 15:52:05 +0100

    added quick references to table values

--- planet.py
+++ planet.py
@@ -436,8 +436,8 @@ def jupiter(its,phys_consts,z,z_ext,x,ghosts):
     
     #Field aligned currents
 #    idx = (np.abs(z - radius/2)).argmin() #find index of closest point to 1 Rj
-    FAC = 3e-11* (A[2]/A) # 1-7 microAm-1 from Ray+ 2009 #why then is it e-11?
-    b_temp = 700 #700 for run 1, 200 for subauroral
+    FAC = 3e-10 *(A[2]/A) # 1-7 microAm-1 from Ray+ 2009 # 7 for auroral, 3 for run 1
+    b_temp = 700 #700 for run 1, 200 for subauroral and nonauroral, 2000 for auroral
     
     # ----------ION TEMPERATURE-------------
     #Initial H+ temperature profile and hence pressure (again log was a test I think so needs investigating again)
@@ -468,20 +468,20 @@ def jupiter(its,phys_consts,z,z_ext,x,ghosts):
     u_H3_plus[-2,0]=pw.T2v(T_H3_plus[-2,0],m_H3_plus)
     
     # ----------ION DENSITY-------------
-    n_H_plus_floor = 6*10**7##4*10**5
-    n_H_plus[2:-2,0] = 2*10**9*np.exp(-0.5*(z/z[0])) + n_H_plus_floor#+ 6*10**7#8*10**4
-    n_H_plus[0:2,0] =2*10**9*np.exp(-0.5*(gb_z/z[0])) + n_H_plus_floor#+ 6*10**7#8*10**4
-    n_H_plus[-2:,0]=2*10**9*np.exp(-0.5*(ge_z/z[0]))+ n_H_plus_floor#+ 6*10**7#8*10**4
-    rho_H_plus[2:-2,0] = n_H_plus[2:-2,0] * m_H_plus # slightly off
+    n_H_plus_floor = 6*10**7#6*10**7#4*10**5#6e4
+    n_H_plus[2:-2,0] = 2*10**9*np.exp(-0.5*(z/z[0])) + n_H_plus_floor #2*10**9 (subauroral, run 1) #1*10**10 (auroral), #5*10**8 (nonauroral)
+    n_H_plus[0:2,0] =2*10**9*np.exp(-0.5*(gb_z/z[0])) + n_H_plus_floor
+    n_H_plus[-2:,0]=2*10**9*np.exp(-0.5*(ge_z/z[0]))+ n_H_plus_floor
+    rho_H_plus[2:-2,0] = n_H_plus[2:-2,0] * m_H_plus 
     rho_H_plus[0:2,0]=n_H_plus[0:2,0] * m_H_plus
     rho_H_plus[-2:,0]=n_H_plus[-2:,0] * m_H_plus
 
     #initial H3+ ion density - exponential decrease place holder
-    n_H3_plus_floor = + 10**8##5.5*10**5
-    n_H3_plus[2:-2,0] = 1*10**10*np.exp(-0.4*(z/z[0])) + n_H3_plus_floor
+    n_H3_plus_floor = 10**8#10**8#5.5*10**5#10e4
+    n_H3_plus[2:-2,0] = 1*10**10*np.exp(-0.4*(z/z[0])) + n_H3_plus_floor #1*10**10 (subauroral, run 1) #5*10**10 (auroral) #1*10**9 (nonauroral)
     n_H3_plus[0:2,0]=1*10**10*np.exp(-0.4*(gb_z/z[0])) + n_H3_plus_floor
     n_H3_plus[-2:,0]=1*10**10*np.exp(-0.4*(ge_z/z[0])) + n_H3_plus_floor
-    rho_H3_plus[2:-2,0] = n_H3_plus[2:-2,0] * m_H3_plus # slightly off
+    rho_H3_plus[2:-2,0] = n_H3_plus[2:-2,0] * m_H3_plus
     rho_H3_plus[0:2,0]=n_H3_plus[0:2,0] * m_H3_plus
     rho_H3_plus[-2:,0]=n_H3_plus[-2:,0] * m_H3_plus
      
@@ -502,25 +502,25 @@ def jupiter(its,phys_consts,z,z_ext,x,ghosts):
       
     # ----------NEUTRAL DENSITY-------------
     #initial neutral H2 density - CONSTANT
-    n_H2[2:-2] = (10**10)*10**6*np.exp(-0.3*(z/z[0])) + 10000000#7 should be 6
-    n_H2[0:2]=(10**10)*10**6*np.exp(-0.3*(gb_z/z[0])) + 10000000
-    n_H2[-2:]=(10**10)*10**6*np.exp(-0.3*(ge_z/z[0])) + 10000000
+    n_H2[2:-2] = 10e16 *np.exp(-0.3*(z/z[0])) + 10000000#7 should be 6 #+ 0(10**10)*10**6
+    n_H2[0:2]=10e16*np.exp(-0.3*(gb_z/z[0])) + 10000000
+    n_H2[-2:]=10e16*np.exp(-0.3*(ge_z/z[0])) + 10000000
     rho_H2[2:-2] = n_H2[2:-2] * m_H2
     rho_H2[0:2]=n_H2[0:2] * m_H2
     rho_H2[-2:]=n_H2[-2:] * m_H2
     
     #initial neutral H density - CONSTANT
-    n_H[2:-2] = (10**9)*10**6*np.exp(-0.3*(z/z[0])) + 1000000#7 should be 6
-    n_H[0:2]=(10**9)*10**6*np.exp(-0.3*(gb_z/z[0])) + 1000000
-    n_H[-2:]=(10**9)*10**6*np.exp(-0.3*(ge_z/z[0])) + 1000000
+    n_H[2:-2] = 10e15*np.exp(-0.3*(z/z[0])) + 10000000#7 should be 6 #(10**9)*10**6
+    n_H[0:2]=10e15*np.exp(-0.3*(gb_z/z[0])) + 10000000
+    n_H[-2:]=10e15*np.exp(-0.3*(ge_z/z[0])) + 10000000
     rho_H[2:-2] = n_H[2:-2] * m_H
     rho_H[0:2]=n_H[0:2] * m_H
     rho_H[-2:]=n_H[-2:] * m_H
    
     #initial neutral He density - CONSTANT
-    n_He[2:-2] = 10**8*10**6*np.exp(-0.5*(z/z[0])) + 10000000 #7 should be 6
-    n_He[0:2]=10**8*10**6*np.exp(-0.5*(gb_z/z[0])) + 10000000
-    n_He[-2:]=10**8*10**6*np.exp(-0.5*(ge_z/z[0])) + 10000000
+    n_He[2:-2] = 10e14*np.exp(-0.5*(z/z[0])) + 100000000 #7 should be 6 #10**8*10**6
+    n_He[0:2]=10e14*np.exp(-0.5*(gb_z/z[0])) + 100000000
+    n_He[-2:]=10e14*np.exp(-0.5*(ge_z/z[0])) + 100000000
     rho_He[2:-2] = n_He[2:-2] * m_He
     rho_He[0:2]=n_He[0:2] * m_He
     rho_He[-2:]=n_He[-2:] * m_He
