2e2b6321c8d24eb1f81f2ca68cb4398ef635614d
Hannah Joyce <93941057+HJoyce1@users.noreply.github.com>
2022-06-22 16:53:38 +0100

    Update command_line_replacement.py

--- command_line_replacement.py
+++ command_line_replacement.py
@@ -1,8 +1,9 @@
 #Code written by H. Joyce (2021)
 #Module designed to replace the command line if desired
 #=========================================================
-
-from pw_1Dsinglefieldline_FAC import polar_wind_outflow as pwm
+from pw_1Dsinglefieldline_asymmetries import bulk_outflow as asym_out
+from pw_1Dsinglefieldline_FAC import bulk_outflow as FAC_out
 
 # planet, time step, iterations, distance (RJ), Lshell, FAC?, CF?, plots?, saves?, name of file
-pwm('jupiter',0.01,10000,3,25,0,0,1,1,'FAC1_dusk_700k')
+# FAC = 0 yes, 1 no, CF = 1 yes, 0 no, plots 1 yes, 0 no, saves yes 1, 0 no
+asym_out('jupiter',0.01,10000,2,25,0,0,1,1,'test')
