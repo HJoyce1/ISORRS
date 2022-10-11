c7fbeba4c5b65e9e96a6c6f9fdaf744b6dbacb7a
Hannah Joyce <93941057+HJoyce1@users.noreply.github.com>
2022-10-11 14:24:46 +0100

    Update command_line_replacement.py

--- command_line_replacement.py
+++ command_line_replacement.py
@@ -1,23 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 22 11:57:07 2021
+# replacement module for the command line
 
-@author: joyceh1
-"""
+# main file with up to date commentary and working functions
+from ISORRS_1Dsinglefieldline import bulk_outflow as ISORRS
 
-#import pw_20190502_hannahedit as pw_HJ
-from pw_1Dsinglefieldline_march2 import polar_wind_outflow as pwm
-from pw_20190502 import polar_wind_outflow as pwm2
-from pw_1Dsinglefieldline_FAC import polar_wind_outflow as pwm3
+# current work-in-progress file used to test new features
+# from ISORRS_1Dsinglefieldline_lax_WIP import bulk_outflow as ISORRS
 
-# planet, time step, iterations, distance (RJ), Lshell, FAC?, CF?, plots?, saves?, name of file
-#pw_HJ.polar_wind_outflow('jupiter',0.01,10000,3,33,0,0,1,1,'TestA')
-#pwm('jupiter',0.01,10000,3,24,0,0,1,1,'breakpoint')
-#pwm2('jupiter',0.01,10000,3,24,0,0,1,1,'pw_2019test')
-pwm3('jupiter',0.01,10000,3,24,0,0,1,1,'results_test_8')
-
-'''
-NOTES:
-    
-
-'''
+# planet name, time step, iterations, distance (RJ), Lshell, FAC?, CF?, plots?, saves?, name of file
+# FAC = 0 yes, 1 no, CF = 1 yes, 0 no, plots 1 yes, 0 no, saves yes 1, 0 no
+ISORRS('jupiter',0.01,10000,1.5,25,0,0,1,1,'test')
