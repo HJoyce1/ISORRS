8dffb772fba14e4ec5d2b246958f5dc9fb0a162c
Hannah Joyce <93941057+HJoyce1@users.noreply.github.com>
2025-02-06 15:04:41 +0000

    update just in case

--- ISORRS_dipolefield.py
+++ ISORRS_dipolefield.py
@@ -6,13 +6,13 @@ Assumes alignment of spin and magnetic dipoles (will change this in future versi
 @author: dconstable
 """
 
-# radius, radius
+# radius, radius 
 def dipolefield(rE,mE,b0,lshell,period,spin_dipole_offset,numpoints,saveflag):
     # import necessary modules
     import numpy as np
     import matplotlib.pyplot as plt
     import scipy.constants
-
+    
     #Values for testing - Earth
 #    rE = 6.371e6
 #    b0 = 3.12e-5
@@ -31,9 +31,9 @@ def dipolefield(rE,mE,b0,lshell,period,spin_dipole_offset,numpoints,saveflag):
 #    numpoints = 1001
 #    spin_dipole_offset = 10
 #    period = (9+(56/60))*3600
-
+    
     omega = 2*np.pi/period                            #Angular frequency of planet rotation
-
+    
     #Calculation of relevant positions and angles
     latE = np.rad2deg(np.arccos(np.sqrt(1/lshell)))   #Latitude of L-shell crossing in degrees
     lat_deg = np.linspace(0,latE,numpoints)           #Linear array of latitudes in degrees
@@ -43,33 +43,33 @@ def dipolefield(rE,mE,b0,lshell,period,spin_dipole_offset,numpoints,saveflag):
     x = r* np.cos(lat_rad)                            #x axis aligned with equator of dipole
     y = r* np.sin(lat_rad)                            #y axis aligned with dipole axis
     phi = np.arctan(np.gradient(y)/np.gradient(x))    #Phi is angle of the field line wrt equator
-
+    
     #Calculation of dipole magnetic field
     bmag = (b0/lshell**3)*np.sqrt(1+3*np.sin(lat_rad)**2)/np.cos(lat_rad)**6
     dB = np.gradient(bmag)
-
+    
     #Plot to check magnetic field
-#    plt.plot(r_norm, bmag, '.'),
-#    plt.xlabel('r [r$_E$]'),
-#    plt.ylabel('B-Field [T]'),
+#    plt.plot(r_norm, bmag, '.'), 
+#    plt.xlabel('r [r$_E$]'), 
+#    plt.ylabel('B-Field [T]'), 
 #    plt.xlim([np.min(r_norm),np.max(r_norm)])
-
+    
     #Calculate length of individual segments of field line, ds
     dlat_rad = np.gradient(lat_rad)                   #Gradient in points in latitude (radians)
     dr = np.gradient(r)                               #Gradient in radial position
     ds = np.zeros(np.size(r))                         #Zeros for ds
-    for i in range(0,np.size(ds)):
+    for i in range(0,np.size(ds)):                    
         ds[i] = np.sqrt(dr[i]**2 + r[i]**2*dlat_rad[i]**2)
-
-    #Calculate sum of all segment lengths to get 1-D z-axis for field line
+    
+    #Calculate sum of all segment lengths to get 1-D z-axis for field line  
     z = np.zeros(np.size(ds))
     for i in range(0,np.size(z)):
         z[i] = sum(ds[0:i])
-
+    
     #Calculate field aligned acceleration due to gravity and centrifugal accelerations
     ag = (scipy.constants.G*mE/r**2)* np.cos(phi)
     ac = omega**2 * -r * np.cos(phi)
-
+    
     if saveflag == 1:
         np.savetxt('grav_data.dat',ag)
         np.savetxt('centrifugal_data.dat',ac)
@@ -85,7 +85,7 @@ def dipolefieldlimits(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpoi
     import numpy as np
     import matplotlib.pyplot as plt
     import scipy.constants
-
+    
     #Values for testing - Earth
 #    rE = 6.371e6
 #    b0 = 3.12e-5
@@ -106,7 +106,7 @@ def dipolefieldlimits(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpoi
 #    numpoints = 1001
 #    spin_dipole_offset = 10
 #    period = (9+(56/60))*3600
-
+    
     #Values for testing - Saturn
 #    rE = 5.8232e7
 #    rmin = 1400000
@@ -117,9 +117,9 @@ def dipolefieldlimits(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpoi
 #    numpoints = 800
 #    spin_dipole_offset = 0
 #    period = (10+(42/60))*3600
-
+    
     omega = 2*np.pi/period                            #Angular frequency of planet rotation
-
+    
     #Calculation of relevant positions and angles
     latE = np.rad2deg(np.arccos(np.sqrt(1/lshell)))   #Latitude of L-shell crossing in degrees
     lat_deg = np.linspace(0,latE,numpoints)           #Linear array of latitudes in degrees
@@ -128,7 +128,7 @@ def dipolefieldlimits(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpoi
     indices = np.where((r_old > rmin) & (r_old < rmax))
     lat_rad = lat_rad_old[indices]
     lat_rad = np.linspace(np.min(lat_rad),np.max(lat_rad),numpoints)
-    r = lshell*rE*np.cos(lat_rad)**2
+    r = lshell*rE*np.cos(lat_rad)**2  
     r_norm = r/rE                                     #Radial position norm. to planet radius
     x = r* np.cos(lat_rad)                            #x axis aligned with equator of dipole
     y = r* np.sin(lat_rad)                            #y axis aligned with dipole axis
@@ -138,34 +138,34 @@ def dipolefieldlimits(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpoi
     xnew = np.linspace(np.min(x_old),np.max(x_old),numpoints)
     ynew = np.linspace(np.max(y_old),np.max(y_old),numpoints)
     phi = np.arctan(np.gradient(y)/np.gradient(x))    #Phi is angle of the field line wrt equator
-
-
+   
+    
     #Calculation of dipole magnetic field
     bmag = (b0/lshell**3)*np.sqrt(1+3*np.sin(lat_rad)**2)/np.cos(lat_rad)**6
     dB = np.gradient(bmag)
-
+    
     #Plot to check magnetic field
-#    plt.plot(r_norm, bmag, '.'),
-#    plt.xlabel('r [r$_E$]'),
-#    plt.ylabel('B-Field [T]'),
+#    plt.plot(r_norm, bmag, '.'), 
+#    plt.xlabel('r [r$_E$]'), 
+#    plt.ylabel('B-Field [T]'), 
 #    plt.xlim([np.min(r_norm),np.max(r_norm)])
-
+    
     #Calculate length of individual segments of field line, ds
     dlat_rad = np.gradient(lat_rad)                   #Gradient in points in latitude (radians)
     dr = np.gradient(r)                               #Gradient in radial position
     ds = np.zeros(np.size(r))                         #Zeros for ds
-    for i in range(0,np.size(ds)):
+    for i in range(0,np.size(ds)):                    
         ds[i] = np.sqrt(dr[i]**2 + r[i]**2*dlat_rad[i]**2)
-
-    #Calculate sum of all segment lengths to get 1-D z-axis for field line
+    
+    #Calculate sum of all segment lengths to get 1-D z-axis for field line  
     z = np.zeros(np.size(ds))
     for i in range(0,np.size(z)):
         z[i] = sum(ds[0:i])
-
+    
     #Calculate field aligned acceleration due to gravity and centrifugal accelerations
     ag = (scipy.constants.G*mE/r**2)* np.cos(phi)
     ac = omega**2 * -r * np.cos(phi)
-
+    
     if saveflag == 1:
         np.savetxt('grav_data.dat',ag)
         np.savetxt('cent_data.dat',ac)
@@ -181,7 +181,7 @@ def dipolefield_ISORRS(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpo
     # import needed modules
     import numpy as np
     import scipy.constants
-
+    
     #Values for testing - Earth
 #    rE = 6.371e6
 #    b0 = 3.12e-5
@@ -202,7 +202,7 @@ def dipolefield_ISORRS(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpo
 #    numpoints = 1001
 #    spin_dipole_offset = 10
 #    period = (9+(56/60))*3600
-
+    
     #Values for testing - Saturn
 #    rE = 5.8232e7
 #    rmin = 1400000
@@ -216,13 +216,13 @@ def dipolefield_ISORRS(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpo
 
     # --------- CALCULATIONS ---------
     # generates field line based on input variables
-    # generates a point for each point on the grid
+    # generates a point for each point on the grid 
     # then generates angle of field line to equator in order to calculate the
     # gravitational acceleration and centrifugal acceleration generated due to
     # this field line positioning
-
+    
     omega = 2*np.pi/period                            # angular frequency of planet rotation
-
+    
     # calculation of relevant positions and angles
     latE = np.rad2deg(np.arccos(np.sqrt(1/lshell)))   # latitude of L-shell crossing in degrees
     lat_deg = np.linspace(latE,0,numpoints)           # linear array of latitudes in degrees (counting downward from ~70)
@@ -231,14 +231,16 @@ def dipolefield_ISORRS(rE,rmin,rmax,mE,b0,lshell,period,spin_dipole_offset,numpo
     indices = np.where((r_old > rmin) & (r_old < rmax))
     lat_rad = lat_rad_old[indices]
     lat_rad = np.linspace(np.max(lat_rad),np.min(lat_rad),numpoints)
+    # print(lat_rad)
+    # breakpoint()
     r = lshell*rE*np.cos(lat_rad)**2
     x = r* np.cos(lat_rad)                            # x axis aligned with equator of dipole
     y = r* np.sin(lat_rad)                            # y axis aligned with dipole axis
     phi = np.arctan(np.gradient(y)/np.gradient(x))    # phi is angle of the field line wrt equator
-
+    
     # calculate field aligned acceleration due to gravity and centrifugal accelerations
     ag = (scipy.constants.G*mE/r**2)* np.cos(phi)
     ac = omega**2 * r * np.cos(phi)
-
+    
     # return variables needed for main module
     return phi,ag,ac
